397e580078cbdde57d8c991c15757dacca2d8a3b
Tamara Margarita Gorosito <90740@sistemas.frc.utn.edu.ar>
2025-11-16 16:31:04 -0300

    Agrega archivos para bds

--- tpi-backend/requests-service/src/main/java/com/tpi/backend/requests_service/repositories/ClienteRepository.java
+++ tpi-backend/requests-service/src/main/java/com/tpi/backend/requests_service/repositories/ClienteRepository.java
@@ -1,6 +1,6 @@
 package com.tpi.backend.requests_service.repositories;
 
-import com.tpi.backend.requests.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.tpi.backend.requests_service.models.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
